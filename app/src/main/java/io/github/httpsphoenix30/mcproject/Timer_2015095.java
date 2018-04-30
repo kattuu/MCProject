93745d059d5ec009579961605d2c780292b6860e
rohit <lifecoderohit@gmail.com>
2018-04-30 13:45:14 +0530

    Clock added.

--- app/src/main/java/io/github/httpsphoenix30/mcproject/Timer_2015095.java
+++ app/src/main/java/io/github/httpsphoenix30/mcproject/Timer_2015095.java
@@ -1,13 +1,18 @@
 package io.github.httpsphoenix30.mcproject;
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import android.app.DialogFragment;
 import android.graphics.Color;
@@ -28,77 +33,112 @@ import com.mcsoft.timerangepickerdialog.RangeTimePickerDialog;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
+import java.util.Calendar;
 import java.util.List;
 
 /**
  * Created by vishal on 28/04/18.
  */
 
-public class Timer_2015095 extends Fragment implements RangeTimePickerDialog.ISelectedTime {
-    private static final int MY_FRAGMENT_ID = 100;
-    private Button clock;
+public class Timer_2015095 extends Fragment {
+    static final int TIME_DIALOG_ID = 1111;
+    private TextView view_start;
+    private TextView view_end;
+    public Button btnClick_start;
+    public Button btnClick_end;
+    private int flag = -1;
+    private int hr;
+    //private int hr_end;
+    private int min;
+    //private int min_end;
 
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle){
+    public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle bundle) {
 
-        View view =inflater.inflate(R.layout.fragment_timer_2015095,viewGroup,false);
+        View frag_view = inflater.inflate(R.layout.fragment_timer_2015095, viewGroup, false);
 
-        clock =view.findViewById(R.id.clock);
-        clock.setOnClickListener(new View.OnClickListener() {
+        view_start = (TextView) frag_view.findViewById(R.id.output_start);
+        view_end = (TextView) frag_view.findViewById(R.id.output_end);
+        final Calendar c = Calendar.getInstance();
+        hr = c.get(Calendar.HOUR_OF_DAY);
+        min = c.get(Calendar.MINUTE);
+        updateTime(hr, min);
+        addButtonClickListener(frag_view);
+
+        return frag_view;
+    }
+
+    public void addButtonClickListener(View view) {
+        btnClick_start = (Button) view.findViewById(R.id.btnClick_start);
+        btnClick_end = (Button) view.findViewById(R.id.btnClick_end);
+        btnClick_start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showCustomDialogTimePicker();
+                flag = 0;
+                createdDialog(TIME_DIALOG_ID).show();
             }
         });
-
-        return view;
-    }
-
-    public void showCustomDialogTimePicker()
-    {
-        // Create an instance of the dialog fragment and show it
-        RangeTimePickerDialog dialog = new RangeTimePickerDialog();
-        dialog.newInstance();
-        dialog.setIs24HourView(false);
-        dialog.setRadiusDialog(20);
-        dialog.setTextTabStart("Start");
-        dialog.setTextTabEnd("End");
-        dialog.setTextBtnPositive("Accept");
-        dialog.setTextBtnNegative("Close");
-        dialog.setValidateRange(false);
-        dialog.setColorBackgroundHeader(R.color.colorPrimary);
-        dialog.setColorBackgroundTimePickerHeader(R.color.colorPrimary);
-        dialog.setColorTextButton(R.color.colorPrimaryDark);
-        android.app.FragmentManager fragmentManager = getActivity().getFragmentManager();
-        dialog.show(fragmentManager, "");
-    }
-
-    @Override
-    public void onSelectedTime(int hourStart, int minuteStart, int hourEnd, int minuteEnd)
-    {
-        Toast.makeText(getActivity(), "Start: "+hourStart+":"+minuteStart+"\nEnd: "+hourEnd+":"+minuteEnd, Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == MY_FRAGMENT_ID)
-        {
-            if (resultCode == Activity.RESULT_OK)
-            {
-                if (data.getExtras().containsKey(RangeTimePickerDialog.HOUR_START))
-                {
-                    int hourStart = data.getExtras().getInt(RangeTimePickerDialog.HOUR_START);
-                    int hourEnd = data.getExtras().getInt(RangeTimePickerDialog.HOUR_END);
-                    int minuteStart = data.getExtras().getInt(RangeTimePickerDialog.MINUTE_START);
-                    int minuteEnd = data.getExtras().getInt(RangeTimePickerDialog.MINUTE_END);
-                    // Use the returned value
-                    Toast.makeText(getActivity(), "Time start:"+hourStart+":"+minuteStart+"\nUntil: "+hourEnd+":"+minuteEnd, Toast.LENGTH_SHORT).show();
-                }
+        btnClick_end.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flag = 1;
+                createdDialog(TIME_DIALOG_ID).show();
             }
+        });
+    }
+
+    protected Dialog createdDialog(int id) {
+        switch (id) {
+            case TIME_DIALOG_ID:
+                return new TimePickerDialog(getActivity(), timePickerListener, hr, min, false);
+        }
+        return null;
+    }
+
+    private TimePickerDialog.OnTimeSetListener timePickerListener = new TimePickerDialog.OnTimeSetListener() {
+        @Override
+        public void onTimeSet(TimePicker view, int hourOfDay, int minutes) {
+// TODO Auto-generated method stub
+            hr = hourOfDay;
+            min = minutes;
+            updateTime(hr, min);
+        }
+    };
+
+    private static String utilTime(int value) {
+        if (value < 10) return "0" + String.valueOf(value);
+        else return String.valueOf(value);
+    }
+
+    private void updateTime(int hours, int mins) {
+        String timeSet = "";
+        if (hours > 12) {
+            hours -= 12;
+            timeSet = "PM";
+        } else if (hours == 0) {
+            hours += 12;
+            timeSet = "AM";
+        } else if (hours == 12)
+            timeSet = "PM";
+        else
+            timeSet = "AM";
+        String minutes = "";
+        if (mins < 10)
+            minutes = "0" + mins;
+        else
+            minutes = String.valueOf(mins);
+        String aTime = new StringBuilder().append(hours).append(':').append(minutes).append(" ").append(timeSet).toString();
+        if(flag == -1) {
+            view_start.setText(aTime);
+            view_end.setText(aTime);
+        }
+        else if(flag == 0) {
+            view_start.setText(aTime);
+        }
+        else if(flag == 1) {
+            view_end.setText(aTime);
         }
     }
-
 }
+
 
